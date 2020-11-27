441200ea3c4ddd99044ba95826723dd5ed4e3619
Esmail <elmoussel@gmail.com>
2020-11-27 23:52:32 +0200

    finish verify code component functionality

--- src/components/verifyCode.js
+++ src/components/verifyCode.js
@@ -1,8 +1,57 @@
 import { Component } from "react";
+import { postService } from "../services/axiosServices";
 
 class verifyCode extends Component {
+  constructor() {
+    super();
+    this.state = {
+      code: "",
+      errorMessage: "",
+      valideCode: false,
+    };
+  }
+
+  onInputChange = (e) => {
+    this.setState({ code: e.target.value });
+  };
+
+  validateCode = () => {
+    const { code } = this.state;
+
+    if (code.length !== 4 || !Number(code)) {
+      return this.setState({ errorMessage: "please enter a 4 digits code!" });
+    }
+
+    postService("/discount/validate", {
+      code: code,
+    })
+      .then((res) => {
+        this.setState({ valideCode: true });
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        if (err.response?.data?.message === "code is not valid!") {
+          return this.setState({ errorMessage: err.response?.data?.message });
+        }
+
+        this.setState({ errorMessage: "something went wrong" });
+      });
+  };
+
   render() {
-    return <span>verifyCode</span>;
+    const { errorMessage, valideCode } = this.state;
+
+    return valideCode ? (
+      <p>Success</p>
+    ) : (
+      <>
+        <input onChange={this.onInputChange} />
+        <button onClick={this.validateCode}>
+          <span>Validate</span>
+        </button>
+        {errorMessage && <p className="error">{errorMessage}</p>}
+      </>
+    );
   }
 }
 
