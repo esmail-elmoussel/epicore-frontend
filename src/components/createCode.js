fb1c5da1008762224002210fd652d28f5c4a0c6e
Esmail <elmoussel@gmail.com>
2020-11-27 23:52:23 +0200

    finish create code component functionality

--- src/components/createCode.js
+++ src/components/createCode.js
@@ -1,8 +1,37 @@
 import { Component } from "react";
+import { postService } from "../services/axiosServices";
 
 class createCode extends Component {
+  constructor() {
+    super();
+    this.state = {
+      code: "",
+      error: false,
+    };
+  }
+
+  generateCode = () => {
+    postService("/discount/create")
+      .then((res) => {
+        this.setState({ code: res.data.code });
+      })
+      .catch(() => {
+        this.setState({ error: true });
+      });
+  };
+
   render() {
-    return <span>createCode</span>;
+    const { code, error } = this.state;
+
+    return (
+      <>
+        <button className="button" onClick={this.generateCode}>
+          <span className="text">Generate Code</span>
+        </button>
+        <p>{code}</p>
+        {error && <p className="error">an error occurred!</p>}
+      </>
+    );
   }
 }
 
